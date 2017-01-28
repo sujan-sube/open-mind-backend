4c884b85b1f11c0fe8b086bf4d837777bdda86fa
Sujan Subendran <ssujan1@hotmail.com>
2017-01-28 15:06:23 -0500

    adjustments to journal model

--- journal/models.py
+++ journal/models.py
@@ -5,6 +5,6 @@ from django.db.models import SET_NULL
 # Create your models here.
 class Journal(models.Model):
   id = models.AutoField(primary_key=True)
-  user = models.ForeignKey('auth.User', null=True, default=None, on_delete=SET_NULL)
+  user = models.ForeignKey('auth.User', null=True, default=None, on_delete=SET_NULL, related_name='author')
   date = models.DateTimeField(default=timezone.now)
   content = models.TextField()
