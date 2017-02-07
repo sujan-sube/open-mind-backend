ac701d29e94cfe6c0852cd51ef67319b90fab919
Sujan Subendran <ssujan1@hotmail.com>
2017-02-07 15:54:01 -0500

    re-added session authentication for testing

--- openmind/settings.py
+++ openmind/settings.py
@@ -135,6 +135,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'PAGE_SIZE': 10
 }
