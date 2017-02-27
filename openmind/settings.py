3b00b82b04aae4f896b0a0a8b0bf7ae199926f4d
Sujan Subendran <ssujan1@hotmail.com>
2017-02-26 21:33:10 -0500

    remove session authentication

--- openmind/settings.py
+++ openmind/settings.py
@@ -137,7 +137,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'PAGE_SIZE': 10
 }
