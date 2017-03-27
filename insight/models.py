684deac34ebc2463ebbe6c8722ae804cf914b809
Sujan Subendran <ssujan1@hotmail.com>
2017-03-27 17:17:50 -0400

    updated model for insight to match frontend

--- insight/models.py
+++ insight/models.py
@@ -4,7 +4,15 @@ from django.db.models import SET_NULL
 
 # Create your models here.
 class Insight(models.Model):
+    TOPIC_CHOICES = (
+        ('emotion', 'Emotion'),
+        ('physical', 'Physical Activity'),
+        ('sleep', 'Sleep Activity'),
+    )
+
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey('auth.User', null=True, default=None, on_delete=SET_NULL)
     date = models.DateTimeField(default=timezone.now)
-    insight = models.TextField()
+    info = models.CharField(max_length=200)
+    detail = models.TextField()
+    topic = models.CharField(max_length=25, choices=TOPIC_CHOICES)
